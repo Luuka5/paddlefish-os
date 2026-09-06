a538dec1e5efcec21ec3999bd287d9de67a3d633
Touko Päivänsalo <touko.paivansalo@gmail.com>
2026-09-06 05:32:58 +0300

    use terminal colors nvim

--- system_files/all/etc/skel/.config/nvim/init.lua
+++ system_files/all/etc/skel/.config/nvim/init.lua
@@ -1,1 +1,10 @@
 -- https://neovim.io/doc/user/
+
+-- Clear current highlighting
+vim.cmd("highlight clear")
+if vim.fn.exists("syntax_on") == 1 then
+  vim.cmd("syntax reset")
+end
+
+-- Ensure termguicolors is disabled so Neovim uses 16-color terminal ANSI palette
+vim.opt.termguicolors = false
