f53a6981ec9bd80bfcc27c5c4e52dcfb911a2c6c
Touko Päivänsalo <touko.paivansalo@gmail.com>
2026-09-07 18:13:42 +0300

    Install FiraCode nerdfont

--- scripts/niri.sh
+++ scripts/niri.sh
@@ -23,3 +23,11 @@ dnf5 install -y \
     xwayland-satellite
 
 dnf5 clean all
+
+# FiraCode Nerd Font: patched Fira Code with icon glyphs.
+curl -sL -o /tmp/FiraCode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.5.1/FiraCode.zip
+unzip -q /tmp/FiraCode.zip -d /tmp/firacode 'FiraCodeNerdFont-*.ttf' 'FiraCodeNerdFontMono-*.ttf'
+install -d -m 0755 /usr/share/fonts/fira-code-nerd-fonts
+install -m 0644 /tmp/firacode/*.ttf /usr/share/fonts/fira-code-nerd-fonts/
+fc-cache -f
+rm -rf /tmp/FiraCode.zip /tmp/firacode
